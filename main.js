d73a00f4795ac1650b35d120b49b42d5e646f202
Yuri Maia <yurirmaia@gmail.com>
2022-11-27 23:15:41 -0300

    auto_rotate

--- main.js
+++ main.js
@@ -25,12 +25,15 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.set(0.4, 0.4, 0.4)
 
+
 const renderer = new THREE.WebGLRenderer()
     renderer.setClearColor(new THREE.Color("#101010"),1);
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.autoRotate = 1
+controls.autoRotateSpeed = -1
 controls.enableDamping = true
 controls.target.set(0, 0.1, 0)
 
@@ -88,27 +91,3 @@ function render() {
 
 animate()
 
-
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
